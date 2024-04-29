07d1651a79d81492b67a0fc97988b39e0c963399
Rezab Ud Dawla <rezabuddawlatuhin@gmail.com>
2024-04-29 23:53:07 +0600

    added soft delete functionality to the task, created some migration for soft delete and for jobs updated some dependencies

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'title',
@@ -23,7 +24,7 @@ class Task extends Model
 
     public function assigneeUser(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'assignee');
+        return $this->belongsTo(User::class, 'assignee', 'id');
     }
 
     public function createdByUser(): BelongsTo
