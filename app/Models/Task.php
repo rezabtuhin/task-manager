2e7a1a5b98d4a77d808f9f377dc3da5ddd392a74
Rezab Ud Dawla <rezabuddawlatuhin@gmail.com>
2024-05-01 00:26:45 +0600

    added necessary function for status change

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -22,6 +22,32 @@ class Task extends Model
         'status',
     ];
 
+    public static array $allowedStatuses = ['New', 'In Progress', 'Testing', 'Deployed'];
+    public function changeStatus($newStatus)
+    {
+        if (in_array($newStatus, self::$allowedStatuses)) {
+            if ($this->status == 'New') {
+                $this->status = $newStatus;
+                $this->save();
+                return true;
+            }
+            else if ($this->status == 'Deployed') {
+                return 'already_deployed';
+            }
+            else {
+                $minutesRemaining = $this->updated_at->diffInMinutes(now());
+                if ($minutesRemaining <= 15) {
+                    return "not_allowed:$minutesRemaining";
+                }
+            }
+            $this->status = $newStatus;
+            $this->save();
+            return true;
+        }
+        return false;
+    }
+
+
     public function assigneeUser(): BelongsTo
     {
         return $this->belongsTo(User::class, 'assignee', 'id');
