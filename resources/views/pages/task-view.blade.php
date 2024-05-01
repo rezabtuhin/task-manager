315a9059eb5f28a7458335b6c9dde0400b09b4aa
Rezab Ud Dawla <rezabuddawlatuhin@gmail.com>
2024-05-01 20:43:52 +0600

    some UI changes in the task view

--- resources/views/pages/task-view.blade.php
+++ resources/views/pages/task-view.blade.php
@@ -20,38 +20,60 @@
             <div>
                 <h1 class="text-[20px] font-black">Images</h1>
                 <div class="flex space-x-2">
+                    @php
+                        $imageFound = false;
+                    @endphp
                     @foreach($cachedTask->images as $image)
                         @if (strpos($image, '.') !== false)
                             @php
                                 $extension = pathinfo($image, PATHINFO_EXTENSION);
                             @endphp
                             @if (in_array($extension, ['jpg', 'jpeg', 'png', 'gif', 'bmp']))
+                                @php
+                                    $imageFound = true;
+                                @endphp
                                 <img src="{{ asset($image) }}" alt="" srcset="" width="100">
                             @endif
                         @endif
                     @endforeach
+                    @if (!$imageFound)
+                        <p>No images provided</p>
+                    @endif
                 </div>
             </div>
             <div>
                 <h1 class="text-[20px] font-black">Files</h1>
                 <ol class="list-decimal">
-                @foreach($cachedTask->images as $image)
                     @php
-                        $fileName = basename($image);
+                        $fileFound = false;
                     @endphp
-                    @if (strpos($image, '.') !== false)
+                    @foreach($cachedTask->images as $image)
                         @php
-                            $extension = pathinfo($image, PATHINFO_EXTENSION);
+                            $fileName = basename($image);
                         @endphp
-                        @if (!in_array($extension, ['jpg', 'jpeg', 'png', 'gif', 'bmp']))
+                        @if (strpos($image, '.') !== false)
+                            @php
+                                $extension = pathinfo($image, PATHINFO_EXTENSION);
+                            @endphp
+                            @if (!in_array($extension, ['jpg', 'jpeg', 'png', 'gif', 'bmp']))
+                                @php
+                                    $fileFound = true;
+                                @endphp
+                                <li><a class="text-blue-700 hover:underline" href="{{ asset($image) }}" download>{{ $fileName }}</a></li>
+                            @endif
+                        @else
+                            @php
+                                $fileFound = true;
+                            @endphp
                             <li><a class="text-blue-700 hover:underline" href="{{ asset($image) }}" download>{{ $fileName }}</a></li>
                         @endif
-                    @else
-                        <li><a class="text-blue-700 hover:underline" href="{{ asset($image) }}" download>{{ $fileName }}</a></li>
+                    @endforeach
+                    @if (!$fileFound)
+                        <p>No files provided</p>
                     @endif
-                @endforeach
                 </ol>
             </div>
+
         </div>
         <div class="space-x-2">
             <a href="/task/{{ $cachedTask->id }}/edit" class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 hover:border-blue-500 rounded text-[19px]">
